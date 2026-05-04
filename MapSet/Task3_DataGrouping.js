d14e01e8b1a4c4542db1096285479a87def40717
qerobyan-van <qerobyanvan@gmail.com>
2026-05-04 23:11:26 +0400

    MapSet

--- MapSet/Task3_DataGrouping.js
+++ MapSet/Task3_DataGrouping.js
@@ -8,11 +8,11 @@ const students = [
 
 function groupByGroup(students) {
     let map = new Map();
-    let arr = [];
 
     for (let keys of students) {
+        let arr= [];
         if (!map.has(keys["group"])) {
-            map.set(keys.group, []);
+            map.set(keys.group, arr);
         }
 
         let res = map.get(keys["group"]);
